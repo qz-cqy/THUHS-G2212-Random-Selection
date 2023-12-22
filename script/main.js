3fe07ec0ee8eff1446eefd0df9c9303706835fc2
rui_er <qz-cqy@outlook.com>
2023-12-22 12:22:11 +0800

    feature: import G2212 namelist

--- script/main.js
+++ script/main.js
@@ -87,3 +87,29 @@ function generate() {
         }, 2000);
     }
 }
+let crypto = require("./crypto.js");
+function md5(str) {
+    return crypto.MD5(str).toString();
+}
+function aesEncrypt(str, key) {
+    var key = crypto.enc.Utf8.parse(key);
+    var srcs = crypto.enc.Utf8.parse(str);
+    var encrypted = crypto.AES.encrypt(srcs, key, { mode: crypto.mode.ECB, padding: crypto.pad.Pkcs7 });
+    return encrypted.toString();
+}
+function aesDecrypt(str, key) {
+    var key = crypto.enc.Utf8.parse(key);
+    var decrypt = crypto.AES.decrypt(str, key, { mode: crypto.mode.ECB, padding: crypto.pad.Pkcs7 });
+    return crypto.enc.Utf8.stringify(decrypt).toString();
+}
+function init_G2212() {
+    let key = document.getElementById("namelist").value;
+    if(md5.md5(key) != "79e3f0902c0cb8f6d4e98dda490cbfee") alert("密码错误！");
+    else {
+        let str = "U2FsdGVkX1+nimrp3Zvh3T3kj8U6q3wGX1eMjJxJgAcV+WB9lr479vezfP5uewfBlWSMhyWPUXPlVz3m1FmyvydgzdMzqGJcxsGvH4cM4YJ5h1oR0D2vNZWC/+NZ7jU/pEVtFWco1pTS3ZxjnuMCin8/6etZfMevdZbsCn3wcWbzhw6zszsthT/Dw5uNnntayyFJgvis8VWkiTaCZ6aVNR5oKQnsnsYPMMDyP36l+N0Dw8R89g1qEDCQJAmMSSuLsJ0BfRPOUmj4UAxe9rAdaltekJ41DBceMOL3VAO9+6207nGp/0J00HorMhMeib6VgoxZmj8NrpmVS45WzrqSPCO+PePGsw88zh/DjLnMvmnCALrs2JXBbOt7HD1qUDKLUqzF9yLZNXjmVxolOcM7KdilvbtEIxeeMVI41bokl4CUzi8SxCpb4uBI4yD2mKdaN0ji0XLHiAnqLpZ+iKWQe+tmw9AZz7vn0Qj52yBdYXWgmE5JN98Y8/MCVo5FPozS6151vUxwDO/tlVg1msywZ12fzECvmiQwli2RPs6Qm1jD4Hkb7I+6KieYGv/gep5c3+VPC/PgpP+5kjdZs/rbomsYL/YRONjRwR/6hC1kqMI=";
+        let namelist = aesDecrypt(str, key);
+        arr = namelist.split(" ");
+        vis = new Array(arr.length).fill(0);
+        load();
+    }
+}
